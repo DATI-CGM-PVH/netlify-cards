a320367f75faa1979bad5a080a54ecbf5cdd62e6
Jhonatan Silva Carvalho <jhonatan.carvalho@PORTOVELHO.RO.GOV.BR>
2023-12-12 11:59:14 -0400

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, RouterOutlet, MainComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['../demo-styling.css'],
 })
 export class AppComponent {
   title = 'angular-quickstart';
