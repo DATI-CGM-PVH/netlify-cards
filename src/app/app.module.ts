fa5c140511d086529a09364b5358a7ddd6457f14
Jhonatan Silva Carvalho <jhonatan.carvalho@PORTOVELHO.RO.GOV.BR>
2023-12-12 12:14:01 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,54 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Card1Component } from './components/card1/card1.component';
+import { MainComponent } from './pages/main/main.component';
+import { Card2Component } from './components/card2/card2.component';
+import { Card3Component } from './components/card3/card3.component';
+import { Card4Component } from './components/card4/card4.component';
+import { Card5Component } from './components/card5/card5.component';
+import { Card6Component } from './components/card6/card6.component';
+import { Card7Component } from './components/card7/card7.component';
+import { Card8Component } from './components/card8/card8.component';
+import { Card9Component } from './components/card9/card9.component';
+import { Card10Component } from './components/card10/card10.component';
+import { Card11Component } from './components/card11/card11.component';
+import { Card12Component } from './components/card12/card12.component';
+import { Card13Component } from './components/card13/card13.component';
+import { Card14Component } from './components/card14/card14.component';
+import { Card15Component } from './components/card15/card15.component';
+import {Card16Component} from "./components/card16/card16.component";
+import {Card17Component} from "./components/card17/card17.component";
+import {Card18Component} from "./components/card18/card18.component";
+import {Card19Component} from "./components/card19/card19.component";
+import {Card20Component} from "./components/card20/card20.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Card1Component,
+    MainComponent,
+    Card2Component,
+    Card3Component,
+    Card4Component,
+    Card5Component,
+    Card6Component,
+    Card7Component,
+    Card8Component,
+    Card9Component,
+    Card10Component,
+    Card11Component,
+    Card12Component,
+    Card13Component,
+    Card14Component,
+    Card15Component,
+    Card16Component,
+    Card17Component,
+    Card18Component,
+    Card19Component,
+    Card20Component,
+
   ],
   imports: [
     BrowserModule,
